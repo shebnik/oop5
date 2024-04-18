0ee79004fb4d330d5dd0852ce1117ba34d282081
Nikita Shebalin <shebalin13@gmail.com>
2024-04-18 17:38:10 +0300

    oop6

--- main.cpp
+++ main.cpp
@@ -1,130 +1,86 @@
 #include <iostream>
 #include <vector>
-#include <pthread.h>
 #include <ctime>
-#include <chrono>
+#include <pthread.h>
 
 using namespace std;
 
-// Function to multiply two matrices
-void multiply_matrices(int **A, int **B, int **C, int size, int start_row, int end_row)
-{
-    for (int i = start_row; i < end_row; i++)
-    {
-        for (int j = 0; j < size; j++)
-        {
-            C[i][j] = 0;
-            for (int k = 0; k < size; k++)
-            {
-                C[i][j] += A[i][k] * B[k][j];
+// Структура для передачі аргументів у потік
+struct thread_args {
+    vector<vector<double>> *A;
+    vector<double> *b;
+    int start_row, end_row;
+};
+
+// Функція, яку виконуватимуть потоки
+void *gaussian_elimination(void *args) {
+    thread_args *targs = static_cast<thread_args *>(args);
+    vector<vector<double>> &A = *targs->A;
+    vector<double> &b = *targs->b;
+    int start_row = targs->start_row;
+    int end_row = targs->end_row;
+    int n = A.size();
+
+    for (int k = 0; k < n; k++) {
+        for (int i = start_row; i < end_row; i++) {
+            if (i != k) {
+                double factor = A[i][k] / A[k][k];
+                for (int j = k; j < n; j++) {
+                    A[i][j] -= factor * A[k][j];
+                }
+                b[i] -= factor * b[k];
             }
         }
     }
+
+    return nullptr;
 }
 
-// Struct to pass arguments to the thread function
-struct thread_data
-{
-    int **A;
-    int **B;
-    int **C;
-    int size;
-    int start_row;
-    int end_row;
-};
+double measure_execution_time(int size, int num_threads) {
+    vector<vector<double>> A(size, vector<double>(size));
+    vector<double> b(size);
 
-// Thread function
-void *thread_function(void *arg)
-{
-    thread_data *data = static_cast<thread_data *>(arg);
-    multiply_matrices(data->A, data->B, data->C, data->size, data->start_row, data->end_row);
-    return NULL;
-}
-
-// Function to measure execution time
-double measure_execution_time(int size, int num_threads)
-{
-    // Initialize matrices
-    int **A = new int *[size];
-    int **B = new int *[size];
-    int **C = new int *[size];
-    srand(static_cast<unsigned int>(time(NULL)));
-    for (int i = 0; i < size; i++)
-    {
-        A[i] = new int[size];
-        B[i] = new int[size];
-        C[i] = new int[size];
-    }
-
-    // Populate matrices with random values
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < size; j++)
-        {
+    // Ініціалізація матриці A та вектора b
+    srand(time(nullptr));
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
             A[i][j] = rand() % 10;
-            B[i][j] = rand() % 10;
         }
+        b[i] = rand() % 10;
     }
 
-    // Create threads and divide work
+    // Створення потоків    
     pthread_t *threads = new pthread_t[num_threads];
-    thread_data *data = new thread_data[num_threads];
+    thread_args *args = new thread_args[num_threads];
     int chunk_size = size / num_threads;
-    int start_row = 0;
-    for (int i = 0; i < num_threads; i++)
-    {
-        data[i].A = A;
-        data[i].B = B;
-        data[i].C = C;
-        data[i].size = size;
-        data[i].start_row = start_row;
-        data[i].end_row = start_row + chunk_size;
-        if (i == num_threads - 1)
-        {
-            data[i].end_row = size;
-        }
-        pthread_create(&threads[i], NULL, thread_function, &data[i]);
-        start_row += chunk_size;
+
+    clock_t start_time = clock();
+
+    for (int i = 0; i < num_threads; i++) {
+        args[i].A = &A;
+        args[i].b = &b;
+        args[i].start_row = i * chunk_size;
+        args[i].end_row = (i == num_threads - 1) ? size : (i + 1) * chunk_size;
+        pthread_create(&threads[i], nullptr, gaussian_elimination, &args[i]);
     }
 
-    // Start timer
-    auto start_time = chrono::high_resolution_clock::now();
-
-    // Wait for threads to finish
-    for (int i = 0; i < num_threads; i++)
-    {
-        pthread_join(threads[i], NULL);
+    // Очікування завершення потоків
+    for (int i = 0; i < num_threads; i++) {
+        pthread_join(threads[i], nullptr);
     }
 
-    // Stop timer and calculate execution time
-    auto end_time = chrono::high_resolution_clock::now();
-    double execution_time = chrono::duration_cast<chrono::microseconds>(end_time - start_time).count() / 1e6;
-
-    // Free memory
-    for (int i = 0; i < size; i++)
-    {
-        delete[] A[i];
-        delete[] B[i];
-        delete[] C[i];
-    }
-    delete[] A;
-    delete[] B;
-    delete[] C;
-    delete[] threads;
-    delete[] data;
+    clock_t end_time = clock();
+    double execution_time = static_cast<double>(end_time - start_time) / CLOCKS_PER_SEC;
 
     return execution_time;
 }
 
-int main()
-{
+int main() {
     int sizes[] = {100, 1000, 2000, 5000};
     int num_threads_values[] = {2, 4, 8, 16, 32};
 
-    for (int size : sizes)
-    {
-        for (int num_thread : num_threads_values)
-        {
+    for (int size : sizes) {
+        for (int num_thread : num_threads_values) {
             cout << "The size of the matrix: " << size << "x" << size << ", number of threads: " << num_thread << endl;
             double execution_time = measure_execution_time(size, num_thread);
             cout << "Execution time: " << execution_time << " s." << endl;
